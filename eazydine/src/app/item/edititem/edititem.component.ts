d04e6ebf753b0af672bf9973a8bbe680524ac0f8
anushri.srinathaithal <shriaithal@gmail.com>
2019-03-13 14:38:33 -0700

    edit item image upload changes

--- eazydine/src/app/item/edititem/edititem.component.ts
+++ eazydine/src/app/item/edititem/edititem.component.ts
@@ -3,9 +3,11 @@ import {Category} from '../../shared/models/category';
 import {Observable} from 'rxjs';
 import {Item} from '../../shared/models/item';
 import {Menu} from '../../shared/models/menu';
+import {FileUpload} from "../../shared/models/fileupload";
 import {ActivatedRoute, Router} from '@angular/router';
 import {ItemService} from '../../shared/services/item.service';
 import {CategoryService} from '../../shared/services/category.service';
+import {FileUploadService} from "../../shared/services/fileupload.service";
 
 @Component({
   selector: 'app-edititem',
@@ -17,8 +19,12 @@ export class EdititemComponent implements OnInit {
   item$: Observable<Item>;
   itemid: number;
   categories$: Observable<Category[]>;
+  selectedFiles: FileList;
+  currentUpload: FileUpload;
+  public imagePath;
+  imgURL: any;
 
-  constructor(private route: ActivatedRoute, private router: Router, private itemService: ItemService, private categoryService: CategoryService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private itemService: ItemService, private categoryService: CategoryService, private fileUploadService: FileUploadService) { }
 
   ngOnInit() {
     // Subscribed
@@ -31,9 +37,7 @@ export class EdititemComponent implements OnInit {
     this.categories$.subscribe( data => console.log(data));
   }
 
-
-
-  updateItem(updateditem: Item): void {
+  saveUpdatedItem(updateditem: Item): void {
     let itemMenu = new Menu();
     itemMenu.id = 2;
     updateditem.menu = itemMenu;
@@ -44,4 +48,35 @@ export class EdititemComponent implements OnInit {
            this.router.navigate(['/menu']);
          });
   }
+
+  detectFiles(event) {
+      this.selectedFiles = event.target.files;
+
+      var reader = new FileReader();
+      this.imagePath = this.selectedFiles.item(0);
+      reader.readAsDataURL(this.selectedFiles.item(0)); 
+      reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
+  
+  upload() {
+    let file = this.selectedFiles.item(0)
+    this.currentUpload = new FileUpload(file);
+    this.currentUpload.name = this.currentUpload.file.name;
+    this.currentUpload.restaurantId = 1;
+    this.currentUpload.menuId = 1;
+    return this.fileUploadService.pushUpload(this.currentUpload);
+  }
+
+  updateItem(updateditem: Item): void {
+    if(this.selectedFiles) {
+      this.upload().subscribe(downloadURL => {
+          updateditem.imagepath = downloadURL;
+          this.saveUpdatedItem(updateditem);
+        })
+    }else {
+      this.saveUpdatedItem(updateditem);
+    }
+  };
 }
